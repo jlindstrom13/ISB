8e9d68d7def2754b76ab1d6588cf85b236069688
jlindstrom13 <jaquelinehildur@gmail.com>
2025-06-30 14:48:40 -0700

    Code to access aact data CT.gov

--- aact_data.py
+++ aact_data.py
@@ -1,19 +1,26 @@
+import zipfile
+import pandas as pd
+
 aact = '20250626'
 
 def readTable(aact, tablename, usefields):
 	usefields.append('nct_id')
-	file = '/jlindstr/clinicaltrials/zips/'+tablename+'.txt'
+	file = '/users/jlindstr/clinicaltrials/zips/'+tablename+'.txt'
 	try:
 		table = pd.read_csv(file, sep='|', usecols=usefields)
 		#print('read', tablename, 'from file', flush=True)
 	except:
-		zipfilename = 'zips/'+aact+'.zip'
+		zipfilename = f'/users/jlindstr/clinicaltrials/zips/{aact}.zip' #'zips/'+aact+'.zip'
 		zf = zipfile.ZipFile(zipfilename)
 		#print('reading', tablename, 'from zip', flush=True)
 		with zf.open(tablename+'.txt') as f:
 			table = pd.read_csv(f, sep='|', usecols=usefields)
 	return table
 
-refs = readTable(aact, 'study_references', fields['nct_id','pmid','reference_type'])
+refs = readTable(aact, 'study_references', ['nct_id','pmid','reference_type'])
 for index, row in refs.iterrows():
 	nctid = row['nct_id']
+
+#testing
+print("Loaded study_references with", len(refs), "rows")
+print(refs.head())

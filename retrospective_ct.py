10d6e75fda207b62e0492301d78d4c61d716e9a6
jlindstrom13 <jaquelinehildur@gmail.com>
2025-07-01 11:51:47 -0700

    Plotted start date and posted date

--- retrospective_ct.py
+++ retrospective_ct.py
@@ -5,6 +5,8 @@
 
 import zipfile
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 aact = '20250626'
 
@@ -23,9 +25,38 @@ def readTable(aact, tablename, usefields):
 	return table
 
 
-start_dates = readTable(aact, 'studies.txt', ['nct_id','study_first_posted_date', 'study_first_posted_date_type', 'start_date', 'start_date_type'])
-for index, row in start_dates.iterrows():
+df = readTable(aact, 'studies', ['nct_id','study_first_posted_date', 'study_first_posted_date_type', 'start_date', 'start_date_type'])
+for index, row in df.iterrows():
 	nctid = row['nct_id']
 
+print(df.head())
 
-print(start_dates.head())
+type= df['study_first_posted_date_type'].value_counts()
+print(f'number of counts for this column{type}')
+
+# so 2/5 of these are estimated first posted dates...
+
+# looking at which are missing start_date or study_first_posted
+missing_start = df['start_date'].isna().sum()
+missing_posted = df['study_first_posted_date'].isna().sum()
+
+print("Trials missing start_date:", missing_start)
+print("Trials missing study_first_posted_date:", missing_posted)
+
+df['start_date'] = pd.to_datetime(df['start_date'], errors='coerce')
+df['study_first_posted_date'] = pd.to_datetime(df['study_first_posted_date'], errors='coerce')
+
+# Drop rows w/ na
+new_df = df.dropna(subset=['start_date', 'study_first_posted_date'])
+
+new_df.plot.scatter(
+	x='start_date', 
+	y='study_first_posted_date',
+	alpha=0.3, 
+	title='Start vs. Posted Date')
+plt.plot(new_df['start_date'], new_df['start_date'], 'r--', label='start = posted')
+
+plt.savefig("dates_plot.png")
+
+latest_start = new_df['start_date'].max()
+print("Latest start date:", latest_start)

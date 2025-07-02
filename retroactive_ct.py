138a86578b9249317209947daae03c20c793a33d
jlindstrom13 <jaquelinehildur@gmail.com>
2025-07-01 17:02:58 -0700

    Created more histograms

--- retroactive_ct.py
+++ retroactive_ct.py
@@ -4,6 +4,7 @@
 import zipfile
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.stats.mstats import winsorize
 
 aact = '20250626'
 
@@ -20,6 +21,17 @@ def readTable(aact, tablename, usefields):
 		with zf.open(tablename+'.txt') as f:
 			table = pd.read_csv(f, sep='|', usecols=usefields)
 	return table
+
+
+def plot_hist(data, column, filename, bins=100, xlabel='', ylabel='', title='', log=False):
+	plt.figure()
+	plt.hist(data[column], bins=100, log=log)
+	plt.axvline(x=0, color='red', linestyle='--', linewidth=1, label='x = 0')
+	plt.xlabel(xlabel)
+	plt.ylabel(ylabel)
+	plt.title(title)
+	plt.savefig(filename)
+	plt.close()
 
 
 df = readTable(aact, 'studies', ['nct_id','study_first_posted_date', 'study_first_posted_date_type', 'start_date', 'start_date_type'])
@@ -60,7 +72,6 @@ plt.savefig("dates_plot.png", dpi=600)
 latest_start = new_df['start_date'].max()
 print("Latest start date:", latest_start)
 
-
 # zoomed in plot
 new_df.plot.scatter(
 	x='start_date', 
@@ -77,21 +88,58 @@ plt.ylim(pd.to_datetime('1980-01-01'), pd.to_datetime('2035-01-01'))
 plt.savefig("zoomed_dates.png", dpi=600)
 plt.close()
 
-new_df['diff_days'] = (new_df['start_date'] - new_df['study_first_posted_date']).dt.days
+new_df['diff_days'] = (new_df['study_first_posted_date'] - new_df['start_date']).dt.days
 
-plt.figure()
-plt.hist(new_df['diff_days'], bins=100)
-plt.xlabel('Start Date - Posted Date (days)')
-plt.ylabel('Number of Trials')
-plt.title('Start vs Posted Date Difference')
-plt.savefig("diff_start_posted")
-plt.close()
+plot_hist(
+    new_df,
+    column='diff_days',
+    filename='diff_start_posted.png',
+    xlabel='Posted Date - Start Date (days)',
+    ylabel='Number of Trials',
+    title='Posted and Start Date Difference'
+)
 
-short_df = new_df[(new_df['diff_days'] > -5000) & (new_df['diff_days'] < 5000)]
-plt.figure()
-plt.hist(short_df['diff_days'], bins=100)
-plt.xlabel('Start Date - Posted Date (days)')
-plt.ylabel('Number of Trials')
-plt.title('Start vs Posted Date Difference No Outliers')
-plt.savefig("diff_start_post_no_outlier")
-plt.close()
+short_df = new_df[(new_df['diff_days'] > -500) & (new_df['diff_days'] < 500)]
+plot_hist(
+    short_df,
+    column='diff_days',
+    filename='diff_start_post_no_outlier.png',
+    xlabel='Posted Date - Start Date (days)',
+    ylabel='Number of Trials',
+    title='Posted and Start Date Difference No Outliers'
+)
+
+# log scale
+plot_hist(
+    short_df,
+    column='diff_days',
+    filename='log_diff_start_post_log.png',
+    xlabel='Posted Date - Start Date (days)',
+    ylabel='Log Number of Trials',
+    title='Posted and Start Date Difference +/- 500 (Log Scale)',
+    log=True
+)
+
+df_120 = new_df[(new_df['diff_days'] > -120) & (new_df['diff_days'] < 120)]
+plot_hist(
+    df_120,
+    column='diff_days',
+    filename='log_diff_start_post_120.png',
+    xlabel='Posted Date - Start Date (days)',
+    ylabel='Log Number of Trials',
+    title='Posted and Start Date Difference +/- 120 (Log Scale)',
+    log=True
+)
+
+
+df_60 = new_df[(new_df['diff_days'] > -60) & (new_df['diff_days'] < 60)]
+
+plot_hist(
+    df_60,
+    column='diff_days',
+    filename='log_diff_start_post_60.png',
+    xlabel='Posted Date - Start Date (days)',
+    ylabel='Log Number of Trials',
+    title='Posted and Start Date Difference +/- 60 (Log Scale)',
+    log=True
+)

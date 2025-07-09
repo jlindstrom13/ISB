78e37042f2c9114de8fbfc1edfef5d94bfa807cf
jlindstrom13 <jaquelinehildur@gmail.com>
2025-07-09 10:57:59 -0700

    No major changes

--- umap_stability_embedding.py
+++ umap_stability_embedding.py
@@ -18,6 +18,10 @@ na_percent = stability_df.isna().mean().sort_values(ascending=False) * 100
 q3 = np.percentile(na_percent, 75)
 stability_df_filtered = stability_df.loc[:, na_percent <= q3]
 
+#make a copy with NCTID:
+#stability_with_id = stability_df_filtered.copy()
+#nct_ids = stability_with_id['nct_id']
+
 # don't include NCTID dtype='object'
 stability_df_no_nct = stability_df_filtered.select_dtypes(include=[np.number])
 
@@ -36,7 +40,6 @@ stability_standardized = scaler.fit_transform(stability_imputed)
 
 # take a subset of first 10,000 to make it faster
 # stability_subset = stability_standardized[:10000]
-
 
 embedding = reducer.fit_transform(stability_standardized)
 #shape_output = embedding.shape

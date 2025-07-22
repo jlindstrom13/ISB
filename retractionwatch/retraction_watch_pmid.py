4e9370ce4a56e887dabb0d097dcb85fce90c24f3
jlindstrom13 <jaquelinehildur@gmail.com>
2025-07-22 10:39:25 -0700

    No notable changes

--- retractionwatch/retraction_watch_pmid.py
+++ retractionwatch/retraction_watch_pmid.py
@@ -5,3 +5,7 @@ df = pd.read_csv("retraction_watch.csv")
 print(df[['RetractionPubMedID', 'OriginalPaperPubMedID', 'Reason']].head())
 
 print(df.columns)
+
+count_with_pmid = df['OriginalPaperPubMedID'].notnull().sum()
+
+print(f"Number of entries with OriginalPaperPubMedID: {count_with_pmid}")

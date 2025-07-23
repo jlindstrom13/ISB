a8536702f9c9f060ee0bbd0ab6caa269c628563f
jlindstrom13 <jaquelinehildur@gmail.com>
2025-07-23 16:31:47 -0700

    No notable changes

--- retractionwatch/retraction_watch_pmid.py
+++ retractionwatch/retraction_watch_pmid.py
@@ -9,3 +9,6 @@ print(df.columns)
 count_with_pmid = df['OriginalPaperPubMedID'].notnull().sum()
 
 print(f"Number of entries with OriginalPaperPubMedID: {count_with_pmid}")
+
+
+pd.Series( df['OriginalPaperPubMedID']).to_pickle("retracted_pmids.pkl")

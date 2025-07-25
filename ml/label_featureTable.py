bb92c7c98b8ef6b607d533c8f0a8522a6add24c3
jlindstrom13 <jaquelinehildur@gmail.com>
2025-07-25 08:58:01 -0700

    Removed seaborn package, not needed

--- ml/label_featureTable.py
+++ ml/label_featureTable.py
@@ -6,7 +6,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, accuracy_score
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
-import seaborn as sns
 import random
 
 

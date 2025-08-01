d9d6c254c157b9a41ab31be00d3e4c1ab7c4a613
jlindstrom13 <jaquelinehildur@gmail.com>
2025-08-01 15:02:08 -0700

    Histogram of stability features before transformation

--- stability_transformation/stability_g_acronym.py
+++ stability_transformation/stability_g_acronym.py
@@ -8,6 +8,8 @@ from matplotlib.colors import LinearSegmentedColormap
 from matplotlib.ticker import FuncFormatter
 from matplotlib.backends.backend_pdf import PdfPages
 import math
+import seaborn as sns
+
 
 stability_df = pd.read_csv('/15TB_2/gglusman/clinicaltrials/stability.txt.gz', sep='\t')
 
@@ -17,21 +19,21 @@ nct_ids = stability_df['nctid'].reset_index(drop=True) # just nct_ids
 stability_df_no_nct = stability_df.select_dtypes(include=[np.number])
 
 # function to transform
-def median_rescale_to_95(col):
-    median_val = col.median() # should avoid NaNs automatically
-    average_val=col.mean()
-    if median_val <= 0:
-        return col  # skip transformation if median less than or = 0
-    if median_val == 1: # skip transform if median=1
-         power_avg = np.log(0.95) / np.log(average_val)
-         return col ** power_avg
-    power = np.log(0.95) / np.log(median_val)
-    return col ** power
+# def median_rescale_to_95(col):
+#     median_val = col.median() # should avoid NaNs automatically
+#     average_val=col.mean()
+#     if median_val <= 0:
+#         return col  # skip transformation if median less than or = 0
+#     if median_val == 1: # skip transform if median=1
+#          power_avg = np.log(0.95) / np.log(average_val)
+#          return col ** power_avg
+#     power = np.log(0.95) / np.log(median_val)
+#     return col ** power
 
-stability_transformed = stability_df_no_nct.apply(median_rescale_to_95)
+# stability_transformed = stability_df_no_nct.apply(median_rescale_to_95)
 
-# save stablity_transformed to pickle
-stability_transformed.to_pickle("stability_transformed.pkl")
+# # save stablity_transformed to pickle
+# stability_transformed.to_pickle("stability_transformed.pkl")
 
 
 # Save all plots to pdf
@@ -49,5 +51,21 @@ stability_transformed.to_pickle("stability_transformed.pkl")
 #         plt.ylabel('Count (log scale)')
 #         pdf.savefig()
 #         plt.close()
+
+
+pdf_path = "no_transform_histograms.pdf"
+
+with PdfPages(pdf_path) as pdf:
+    for col in stability_df_no_nct.columns:
+       
+        plt.figure(figsize=(6,4))
+        sns.histplot(stability_df_no_nct[col], bins=100, kde=False, color='green')
+        plt.yscale("log")
+        plt.title(f'Pre-Transformation Histograms: {col}')
+        plt.xlim(0,1)
+        plt.xlabel('Value')
+        plt.ylabel('Count (log scale)')
+        pdf.savefig()
+        plt.close()
 
 

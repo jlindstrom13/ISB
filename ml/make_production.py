c308c399b8d824f564e33e23cc57de4f2c6f8162
jlindstrom13 <jaquelinehildur@gmail.com>
2025-08-06 12:08:47 -0700

    Code to check if train and production set are unique

--- ml/make_production.py
+++ ml/make_production.py
@@ -4,19 +4,24 @@ import pandas as pd
 df = pd.read_pickle("featureTable_labeled.pkl")
 matched = pd.read_pickle("matched_cases_controls.pkl")
 
+print(matched.columns)
 # Exclude all NCTs that were used in training (cases + controls)
-training_ncts = set(matched['nct_id'])
+training_ncts = set(matched['cases']).union(set(matched['controls']))
 production_df = df[~df['nct_id'].isin(training_ncts)].copy()
 
-# Confirm it's only unlabeled rows
-assert production_df['label'].isna().all(), "Some labeled trials are in production set!"
-
-# Save production set
+# Save production 
 production_df.to_pickle("production.pkl")
-print(f"Saved production set with {len(production_df)} trials.")
+print(f"Number of trials saved to production: {len(production_df)} ")
 
 
-n_cases = sum(matched['label'] == 1)
-n_controls = sum(matched['label'] == 0)
+
+# Checking to make sure training and production NCTs are unique...
+n_cases = matched['cases'].notna().sum()
+n_controls = matched['controls'].notna().sum()
 print(f"  Cases: {n_cases}")
 print(f"  Controls: {n_controls}")
+print(f" total cases and controls: {n_cases+n_controls}")
+
+
+training_ncts = set(matched['cases']) | (set(matched['controls']))
+print(f"Unique training NCTs: {len(training_ncts)}")

a978578d675bbf4c2b33b430c4b8ba19b95be884
jlindstrom13 <jaquelinehildur@gmail.com>
2025-08-06 15:20:01 -0700

    More code to see why discrepancy in total cases/controls

--- ml/make_production.py
+++ ml/make_production.py
@@ -13,15 +13,27 @@ production_df = df[~df['nct_id'].isin(training_ncts)].copy()
 production_df.to_pickle("production.pkl")
 print(f"Number of trials saved to production: {len(production_df)} ")
 
+# END product of production file
 
 
-# Checking to make sure training and production NCTs are unique...
+# Exploration/ Checking to make sure training and production NCTs are unique...
 n_cases = matched['cases'].notna().sum()
 n_controls = matched['controls'].notna().sum()
-print(f"  Cases: {n_cases}")
-print(f"  Controls: {n_controls}")
+print(f"Cases: {n_cases}")
+print(f"Controls: {n_controls}")
 print(f" total cases and controls: {n_cases+n_controls}")
 
 
 training_ncts = set(matched['cases']) | (set(matched['controls']))
 print(f"Unique training NCTs: {len(training_ncts)}")
+
+cases_set = set(matched['cases'].dropna())
+controls_set = set(matched['controls'].dropna())
+
+overlap_ncts = cases_set & controls_set
+
+
+print(f"Number of NCTs in both cases and controls: {len(overlap_ncts)}")
+
+for nct in list(overlap_ncts)[:5]:
+    print(f"Overlap ex: {nct}")
